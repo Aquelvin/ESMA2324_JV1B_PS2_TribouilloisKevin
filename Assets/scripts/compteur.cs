8519c1ecdf2be69a6c85ac8f9f76d828150c6101
Aquelvin <kevtribouillois@gmail.com>
2024-05-24 08:27:42 +0200

    léger correctif

--- Assets/scripts/compteur.cs
+++ Assets/scripts/compteur.cs
@@ -38,7 +38,7 @@ public class compteur : MonoBehaviour
             cardone = true;
         }
 
-        if (other.CompareTag("porte_1") && crystalcount == 4 && cardone)
+        if (other.CompareTag("porte_1") && crystalcount >= 4 && cardone)
         {
             if (!zone_1_unlock)
             {
